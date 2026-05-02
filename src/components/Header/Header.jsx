5f262040af8c7da40d9e7f6ed6f31d9d8f3d534c
0xGaziin <gaziin208@gmail.com>
2026-05-02 14:50:23 -0300

    style: fixed styles for header and search components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,11 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header className={styles.headerSection}>
-      <h3>Orbital</h3>
-      <Search placeholder='Search for materials, notes...' />
+      <div className={styles.main}>
+        <h3>Orbital</h3>
+        <Search placeholder='Search for materials, notes...' />
+      </div>
+      
 
       <div className={styles.configuration}>
         <button className='notification'>Notifications</button>
