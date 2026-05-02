1fde99caedaef1366d02f67683c18adbe2c98324
0xGaziin <gaziin208@gmail.com>
2026-05-02 12:41:46 -0300

    fix: added Home.module.css to Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
 import Header from "../../components/Header/Header";
+import styles from './Home.module.css';
 
 function Home() {
   return (
     <>
       <Header />
       
-      <section className="hero-section">
-        <h1 className="hero-title">
+      <section className={styles.heroSection}>
+        <h1 className={styles.heroTitle}>
           Imagine the best way to organize your studies
         </h1>
 
-        <p className="hero-subtitle">
+        <p className={styles.heroSubtitle}>
           The complete platform to manage your subjects, notes, and academic progress all in one place.
         </p>
 
-        <div className="actions">
+        <div className={styles.actions}>
           <button>Start Now</button>
           <button>Find Out More</button>
         </div>
