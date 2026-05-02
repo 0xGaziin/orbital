cf2e1850495eb311cd326ac915b9b12dcd889f99
0xGaziin <gaziin208@gmail.com>
2026-05-02 11:38:27 -0300

    feat: added pages to App.jsx and create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+/* Pages */
+import Home from './pages/Home.jsx';
+
+function App() {
+  return (
+    <BroserRouter>
+      <Routes>
+        {/* Main Route */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BroserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <h1>Hello, world! This is the Orbital first page.</h1>
+  )
+}
+
+export default Home;
