0eaffa03ef817d9b445c724427c125b5a1255d70
0xGaziin <gaziin208@gmail.com>
2026-05-02 15:02:11 -0300

    style: added style to Button.jsx component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,9 @@
-function Button() {
-  
+import styles from './Button.module.css';
+
+function Button({ content }) {
+  return (
+    <button className={styles.accessButton}>{content}</button>
+  )  
 }
 
 export default Button;
-
