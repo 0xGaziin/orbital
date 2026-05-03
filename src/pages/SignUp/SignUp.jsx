7db2ad7dacab277b6ef75d2c7f2c976b0e2c89e4
0xGaziin <gaziin208@gmail.com>
2026-05-03 17:35:39 -0300

    feat: added link using react-router-dom

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,6 +2,8 @@ import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import styles from './SignUp.module.css';
 
+import { Link } from "react-router-dom";
+
 function SignUp() {
   return (
     <>
@@ -20,7 +22,7 @@ function SignUp() {
         </form>
 
         <p className={styles.warning}>
-          By creating an account, you agree to our Terms.
+          By creating an account, you agree to our <Link to='/terms'>Terms</Link>.
         </p>
       </div>
     </>
