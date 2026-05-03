8aa5eaf366cc8c346b9268ffad0a016634c0b970
0xGaziin <gaziin208@gmail.com>
2026-05-03 16:50:19 -0300

    feat: added imports to SignUp.jsx page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,14 @@
+import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
+import styles from './SignUp.module.css';
+
 function SignUp() {
   return (
-    <h1>This's the Sign Up page.</h1>
+    <>
+      <Header />
+
+      <h1>This's the Sign Up page.</h1>
+    </>
   )
 }
 
