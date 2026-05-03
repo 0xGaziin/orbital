74b7f74de1e0a556c460a02fbc984d304218899d
0xGaziin <gaziin208@gmail.com>
2026-05-03 17:01:09 -0300

    feat: added base to signup page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -7,7 +7,21 @@ function SignUp() {
     <>
       <Header />
 
-      <h1>This's the Sign Up page.</h1>
+      <div className={styles.registerArea}>
+        <h1>Sign Up</h1>
+        <p>This is your first step.</p>
+
+        <form>
+          <input type="text" placeholder="Full name" />
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="E-mail (optional)" />
+          <input type="password" placeholder="Password"/>
+        </form>
+
+        <p className={styles.warning}>
+          By creating an account, you agree to our Terms.
+        </p>
+      </div>
     </>
   )
 }
